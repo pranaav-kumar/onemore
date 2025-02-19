92f40551b762bee89b7dcbaf8b82324c53e91c73
pranaav kumar <pranaavkumarj@gmail.com>
2025-02-19 21:53:55 +0530

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,196 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color.fromRGBO(225, 213, 201, 1),
-        body: Center(
-          child: Container(
-            height: 100,
-            width: 300,
-            color: Color.fromRGBO(197, 146, 80, 1),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  final Color lightBeige = const Color.fromRGBO(247, 240, 234, 1);
+  final Color warmBeige = const Color.fromRGBO(225, 213, 201, 1);
+  final Color darkGrayBlack = const Color.fromRGBO(34, 35, 37, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBeige,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: darkGrayBlack,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "Login to continue",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: darkGrayBlack.withOpacity(0.7),
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: darkGrayBlack),
+                  filled: true,
+                  fillColor: warmBeige,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: darkGrayBlack),
+                  filled: true,
+                  fillColor: warmBeige,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(color: darkGrayBlack),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: darkGrayBlack,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(color: darkGrayBlack),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: darkGrayBlack,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  final Color lightBeige = const Color.fromRGBO(247, 240, 234, 1);
+  final Color warmBeige = const Color.fromRGBO(225, 213, 201, 1);
+  final Color darkGrayBlack = const Color.fromRGBO(34, 35, 37, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBeige,
+      appBar: AppBar(
+        title: Text("Sign Up", style: TextStyle(color: darkGrayBlack)),
+        backgroundColor: warmBeige,
+        iconTheme: IconThemeData(color: darkGrayBlack),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  filled: true,
+                  fillColor: warmBeige,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  filled: true,
+                  fillColor: warmBeige,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: darkGrayBlack,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
