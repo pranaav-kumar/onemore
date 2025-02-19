6b34ec07e364e2e5db9d01023e8b995046d094c5
pranaav kumar <pranaavkumarj@gmail.com>
2025-02-19 19:55:49 +0530

    login page starting color pallet

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,21 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // this is just for the test
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(225, 213, 201, 1),
+        body: Center(
+          child: Container(
+            height: 100,
+            width: 300,
+            color: Color.fromRGBO(197, 146, 80, 1),
+          ),
+        ),
+      ),
+    );
   }
 }
