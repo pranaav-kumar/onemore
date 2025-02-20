9a2f7e406efd46992f60a10d49c67698651b6463
pranaav kumar <pranaavkumarj@gmail.com>
2025-02-20 14:35:04 +0530

    new signup complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-//second branch
 void main() {
   runApp(const MyApp());
 }
@@ -17,9 +16,10 @@ class MyApp extends StatelessWidget {
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  final Color lightBeige = const Color.fromRGBO(247, 240, 234, 1);
-  final Color warmBeige = const Color.fromRGBO(225, 213, 201, 1);
-  final Color darkGrayBlack = const Color.fromRGBO(34, 35, 37, 1);
+  static const Color lightBeige = Color.fromRGBO(247, 240, 234, 1);
+  static const Color warmBeige = Color.fromRGBO(225, 213, 201, 1);
+  static const Color darkGrayBlack = Color.fromRGBO(34, 35, 37, 1);
+  static final Color darkGrayBlack70 = Color.fromRGBO(34, 35, 37, 0.7);
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +44,6 @@ class LoginPage extends StatelessWidget {
               Text(
                 "Login to continue",
                 style: TextStyle(
-                  fontSize: 16,
                   color: darkGrayBlack.withAlpha((0.7 * 255).toInt()),
                 ),
               ),
@@ -139,13 +138,13 @@ class LoginPage extends StatelessWidget {
 
 class SignUpSelectionPage extends StatelessWidget {
   const SignUpSelectionPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(247, 240, 234, 1),
       appBar: AppBar(
         title: Text("Sign Up", style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
+        backgroundColor: const Color.fromRGBO(225, 213, 201, 1),
         iconTheme: IconThemeData(color: Colors.black),
       ),
       body: Center(
@@ -157,7 +156,19 @@ class SignUpSelectionPage extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => SignUpPage(userType: "Alumni"),
+                    builder:
+                        (context) => SignUpForm(
+                          userType: "Alumni",
+                          fields: [
+                            "Name",
+                            "Mail ID",
+                            "College Passed Out Year",
+                            "Domain of Work",
+                            "Organisation Name",
+                            "Username",
+                            "Password",
+                          ],
+                        ),
                   ),
                 );
               },
@@ -169,7 +180,17 @@ class SignUpSelectionPage extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => SignUpPage(userType: "Student"),
+                    builder:
+                        (context) => SignUpForm(
+                          userType: "Student",
+                          fields: [
+                            "Name",
+                            "Mail ID",
+                            "Department",
+                            "Username",
+                            "Password",
+                          ],
+                        ),
                   ),
                 );
               },
@@ -182,15 +203,91 @@ class SignUpSelectionPage extends StatelessWidget {
   }
 }
 
-class SignUpPage extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
   final String userType;
-  const SignUpPage({super.key, required this.userType});
+  final List<String> fields;
+
+  const SignUpForm({super.key, required this.userType, required this.fields});
+
+  @override
+  SignUpFormState createState() => SignUpFormState();
+}
+
+class SignUpFormState extends State<SignUpForm> {
+  final Map<String, TextEditingController> _controllers = {};
+  bool _isComplete = false;
+
+  @override
+  void initState() {
+    super.initState();
+    for (var field in widget.fields) {
+      _controllers[field] = TextEditingController();
+    }
+  }
+
+  void _checkFields() {
+    setState(() {
+      _isComplete = _controllers.values.every(
+        (controller) => controller.text.isNotEmpty,
+      );
+    });
+  }
+
+  void _showSuccessDialog() {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text("Sign Up Successful"),
+            content: Text("You have successfully signed up!"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                child: Text("OK"),
+              ),
+            ],
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Sign Up as $userType")),
-      body: Center(child: Text("Sign Up Form for $userType")),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children:
+              widget.fields.map((field) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextField(
+                      controller: _controllers[field],
+                      decoration: InputDecoration(labelText: field),
+                      onChanged: (_) => _checkFields(),
+                    ),
+                  );
+                }).toList()
+                ..add(
+                  _isComplete
+                      ? Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: ElevatedButton(
+                          onPressed: _showSuccessDialog,
+                          child: Text("Next"),
+                        ),
+                      )
+                      : Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: Container(),
+                      ),
+                ),
+        ),
+      ),
     );
   }
 }
