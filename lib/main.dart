da7fc3a784af2eb2fdd61abcfe9a0c41470b9b0c
230701234 <230701234@rajalakshmi.edu.in>
2025-03-21 14:09:34 +0530

    committttt

--- lib/main.dart
+++ lib/main.dart
@@ -1,295 +1,179 @@
 import 'package:flutter/material.dart';
 
-//two more change
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
+class AppColors {
   static const Color lightBeige = Color.fromRGBO(247, 240, 234, 1);
   static const Color warmBeige = Color.fromRGBO(225, 213, 201, 1);
   static const Color darkGrayBlack = Color.fromRGBO(34, 35, 37, 1);
   static final Color darkGrayBlack70 = Color.fromRGBO(34, 35, 37, 0.7);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: lightBeige,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Welcome Back",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: darkGrayBlack,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "Login to continue",
-                style: TextStyle(
-                  color: darkGrayBlack.withAlpha((0.7 * 255).toInt()),
-                ),
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  labelStyle: TextStyle(color: darkGrayBlack),
-                  filled: true,
-                  fillColor: warmBeige,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  labelStyle: TextStyle(color: darkGrayBlack),
-                  filled: true,
-                  fillColor: warmBeige,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: darkGrayBlack),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: darkGrayBlack,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                  ),
-                  child: const Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(color: darkGrayBlack),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignUpSelectionPage(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: darkGrayBlack,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
 
-class SignUpSelectionPage extends StatelessWidget {
-  const SignUpSelectionPage({super.key});
+class StudentProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(247, 240, 234, 1),
       appBar: AppBar(
-        title: Text("Sign Up", style: TextStyle(color: Colors.black)),
-        backgroundColor: const Color.fromRGBO(225, 213, 201, 1),
-        iconTheme: IconThemeData(color: Colors.black),
+        title: Text('Student Profile'),
+        backgroundColor: AppColors.darkGrayBlack,
+        automaticallyImplyLeading: false, // Removes debug banner icon
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder:
-                        (context) => SignUpForm(
-                          userType: "Alumni",
-                          fields: [
-                            "Name",
-                            "Mail ID",
-                            "College Passed Out Year",
-                            "Domain of Work",
-                            "Organisation Name",
-                            "Username",
-                            "Password",
-                          ],
-                        ),
-                  ),
-                );
-              },
-              child: Text("Sign Up as Alumni"),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(
+                'assets/profile.jpg',
+              ), // Replace with actual image
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder:
-                        (context) => SignUpForm(
-                          userType: "Student",
-                          fields: [
-                            "Name",
-                            "Mail ID",
-                            "Department",
-                            "Username",
-                            "Password",
-                          ],
-                        ),
-                  ),
-                );
-              },
-              child: Text("Sign Up as Student"),
+            SizedBox(height: 16),
+            Text(
+              'John Doe',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: AppColors.darkGrayBlack,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'XYZ Engineering College',
+              style: TextStyle(fontSize: 18, color: AppColors.darkGrayBlack70),
+            ),
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'About:',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.darkGrayBlack,
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: AppColors.warmBeige,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text(
+                'I am currently working as a software engineer at ABC Corp. Passionate about AI and Machine Learning.',
+                style: TextStyle(fontSize: 16, color: AppColors.darkGrayBlack),
+              ),
+            ),
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Contact Info:',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.darkGrayBlack,
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            ListTile(
+              leading: Icon(Icons.email, color: AppColors.darkGrayBlack),
+              title: Text(
+                'john.doe@example.com',
+                style: TextStyle(color: AppColors.darkGrayBlack),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone, color: AppColors.darkGrayBlack),
+              title: Text(
+                '+123 456 7890',
+                style: TextStyle(color: AppColors.darkGrayBlack),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.link, color: AppColors.darkGrayBlack),
+              title: Text(
+                'LinkedIn: linkedin.com/in/johndoe',
+                style: TextStyle(color: AppColors.darkGrayBlack),
+              ),
+            ),
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Skills:',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.darkGrayBlack,
+                ),
+              ),
+            ),
+            Wrap(
+              spacing: 8,
+              children: [
+                Chip(
+                  label: Text('Flutter'),
+                  backgroundColor: AppColors.warmBeige,
+                ),
+                Chip(label: Text('Dart'), backgroundColor: AppColors.warmBeige),
+                Chip(
+                  label: Text('Machine Learning'),
+                  backgroundColor: AppColors.warmBeige,
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Achievements:',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.darkGrayBlack,
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            ListTile(
+              leading: Icon(Icons.star, color: Colors.amber),
+              title: Text(
+                'Winner of Hackathon 2024',
+                style: TextStyle(color: AppColors.darkGrayBlack),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.star, color: Colors.amber),
+              title: Text(
+                'Best Student Award 2023',
+                style: TextStyle(color: AppColors.darkGrayBlack),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.star, color: Colors.amber),
+              title: Text(
+                'Internship at Google',
+                style: TextStyle(color: AppColors.darkGrayBlack),
+              ),
             ),
           ],
         ),
       ),
+      backgroundColor: AppColors.lightBeige,
     );
   }
 }
 
-class SignUpForm extends StatefulWidget {
-  final String userType;
-  final List<String> fields;
-
-  const SignUpForm({super.key, required this.userType, required this.fields});
-
-  @override
-  SignUpFormState createState() => SignUpFormState();
-}
-
-class SignUpFormState extends State<SignUpForm> {
-  final Map<String, TextEditingController> _controllers = {};
-  bool _isComplete = false;
-
-  @override
-  void initState() {
-    super.initState();
-    for (var field in widget.fields) {
-      _controllers[field] = TextEditingController();
-    }
-  }
-
-  void _checkFields() {
-    setState(() {
-      _isComplete = _controllers.values.every(
-        (controller) => controller.text.isNotEmpty,
-      );
-    });
-  }
-
-  void _showSuccessDialog() {
-    showDialog(
-      context: context,
-      builder:
-          (context) => AlertDialog(
-            title: Text("Sign Up Successful"),
-            content: Text("You have successfully signed up!"),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-                child: Text("OK"),
-              ),
-            ],
-          ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children:
-              widget.fields.map((field) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: TextField(
-                      controller: _controllers[field],
-                      decoration: InputDecoration(labelText: field),
-                      onChanged: (_) => _checkFields(),
-                    ),
-                  );
-                }).toList()
-                ..add(
-                  _isComplete
-                      ? Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: ElevatedButton(
-                          onPressed: _showSuccessDialog,
-                          child: Text("Next"),
-                        ),
-                      )
-                      : Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: Container(),
-                      ),
-                ),
-        ),
-      ),
-    );
-  }
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system, // Enables automatic dark mode switching
+      home: StudentProfilePage(),
+    ),
+  );
 }
