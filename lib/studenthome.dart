b115d7fdc67f94e3c5dafd6143d8a2b8b91b7688
pranaav kumar <pranaavkumarj@gmail.com>
2025-03-21 18:17:34 +0530

    student home page file

---
+++ lib/studenthome.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StudentHome extends StatelessWidget {
+  const StudentHome({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
